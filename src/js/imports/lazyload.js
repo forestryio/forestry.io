74ddc09423706e2044c43eee6fd4d1a1a37ac950
Christopher Macrae <hello@chrisdmacrae.com>
2017-12-22 16:28:52 -0400

    Improve lazyload performance for srcset, cleanup

--- src/js/imports/lazyload.js
+++ src/js/imports/lazyload.js
@@ -8,3 +8,13 @@ const isProduction = process.env.NODE_ENV === "production"
  * handle animations
  * https://www.npmjs.com/package/lazysizes
  */
+document.addEventListener("lazybeforeunveil", (e) => {
+  const srcset = e.target.getAttribute("data-srcset").split(", ")
+  if (srcset[0]) {
+    const replaceRE = new RegExp(".*?[s](.*?w)")
+    const lowestQuality = replaceRE.exec(srcset[0])
+    if (lowestQuality[0]) {
+      e.target.style.src = "url(" + lowestQuality[0] + ")"
+    }
+  }
+})
