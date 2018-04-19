46e8d7523c74ef7ed83f83bdeb9d8fb2e64fb6e5
ncphillips <ncphillips.19@gmail.com>
2018-04-19 16:30:30 -0300

    dynamically load add-site-flow scripts, but it doesn't quite work

---
+++ src/js/load-add-site-flow.js
@@ -0,0 +1,25 @@
+console.log("Load add site flow")
+
+fetch("http://assets.forestry.io/add-site/asset-manifest.json")
+  .then((response) => response.json())
+  .then(addManifestFiles)
+  .catch((e) => console.log("Error", e))
+
+function addManifestFiles(manifest) {
+  addScriptTag(manifest["main.js"])
+  addStylesheet(manifest["main.css"])
+}
+
+function addScriptTag(url) {
+  const el = document.createElement("script")
+  el.setAttribute("src", url)
+  el.setAttribute("type", "text/javascript")
+  document.body.appendChild(el)
+}
+
+function addStylesheet(url) {
+  const el = document.createElement("link")
+  el.setAttribute("href", url)
+  el.setAttribute("rel", "text/css")
+  document.body.appendChild(el)
+}
